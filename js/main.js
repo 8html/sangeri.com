f6c130faa08ecef2ae7b9a238cb6bf3e9287eb50
caiguanhao <caiguanhao@gmail.com>
2014-02-22 21:16:36 +0800

    menu child items should follow parent

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ $(function() {
     menu.find('.menu-c .ms').addClass('hidden');
     var index = $(this).index();
     menu.find('.menu-p a').eq(index).addClass('active');
-    menu.find('.menu-c .ms-' + (index + 1)).removeClass('hidden');
+    var panel = menu.find('.menu-c .ms-' + (index + 1));
+    panel.removeClass('hidden');
+    var totalwidth = 0;
+    panel.find('a').map(function() { totalwidth += $(this).width(); })
+    var maxwidth = panel.width();
+    var center = $(this).position().left + $(this).width() / 2;
+    var left = center - (totalwidth / 2);
+    var offset = 28;
+    left -= offset;
+    if (left < 0) left = 0;
+    if (left + totalwidth > maxwidth) left = maxwidth - totalwidth;
+    panel.find('a:first').css({ 'margin-left': left });
   });
   $('.hompage-slider').sliderkit({
     auto: true,
