ee5ec4532c1de081127a2d953b8293a97cc871cd
caiguanhao <caiguanhao@gmail.com>
2014-02-22 16:57:23 +0800

    improve templating;

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,4 +2,23 @@ module.exports.register = function(Handlebars, options) {
   Handlebars.registerHelper('obj_val', function(object, property) {
     return object ? object[property] : property;
   });
+
+  Handlebars.registerHelper('json_to_str', function(object) {
+    return JSON.stringify(object);
+  });
+
+  Handlebars.registerHelper('get', function(object/* ... */) {
+    for (var i = 1; i < arguments.length - 1; i++) {
+      if (object.hasOwnProperty(arguments[i])) {
+        object = object[arguments[i]];
+      } else {
+        return 'null';
+      }
+    }
+    if (typeof(object) === 'string') {
+      return object;
+    } else {
+      return JSON.stringify(object);
+    }
+  });
 };
