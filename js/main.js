54a00c5587bc72300b4d456d08c50e8103c484e6
caiguanhao <caiguanhao@gmail.com>
2014-03-05 16:44:30 +0800

    add menu bar animation;

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function() {
   var menu = $('.menu').first();
+  var initial = -1;
   menu.find('.mi').hover(function() {
     menu.find('.menu-p a').removeClass('active');
     menu.find('.menu-c .ms').addClass('hidden');
@@ -8,7 +9,10 @@ $(function() {
     var panel = menu.find('.menu-c .ms-' + (index + 1));
     panel.removeClass('hidden');
     var totalwidth = 0;
-    panel.find('a').map(function() { totalwidth += $(this).width(); })
+    var panel_a = panel.find('a');
+    panel_a.map(function() { totalwidth += $(this).width(); });
+    if (initial === -1) initial = panel_a.css('margin-top');
+    panel_a.css({ 'margin-top': menu.find('.menu-c .ms').height(), opacity: 0 });
     var maxwidth = panel.width();
     var center = $(this).position().left + $(this).width() / 2;
     var left = center - (totalwidth / 2);
@@ -16,8 +20,13 @@ $(function() {
     left -= offset;
     if (left + totalwidth > maxwidth) left = maxwidth - totalwidth;
     if (left < 0) left = 0;
-    panel.find('a:first').css({ 'margin-left': left });
-  });
+    panel_a.first().css({ 'margin-left': left });
+    panel_a.each(function(a, b) {
+      $(b).delay(a * 50).animate({ 'margin-top': initial, opacity: 1 }, 'fast');
+    });
+  }, function() {
+
+    });
   $('.hompage-slider').sliderkit({
     auto: true,
     circular: true,
